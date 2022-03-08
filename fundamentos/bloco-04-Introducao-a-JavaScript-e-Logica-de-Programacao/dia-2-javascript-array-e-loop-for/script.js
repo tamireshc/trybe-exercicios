2f14b886b5d243ddc6bd450857a1bb6347dc22aa
tamireshc <tamireshc@gmail.com>
2022-03-08 16:03:35 -0300

    adiciona exercicio 4.2

---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,51 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado
+
+let soma = numbers.reduce((a, b) => a + b);
+console.log(soma);
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado
+let mediaAritimética = soma / numbers.length;
+console.log(mediaAritimética);
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (mediaAritimética > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
+
+//! Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+for (i = 0; i < numbers.length; i += 1) {}
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let odd = numbers.filter((n) => n % 2 !== 0);
+console.log(odd);
+
+//! Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+for (let number of numbers) {
+}
+
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let array1 = [];
+for (i = 1; i <= 25; i += 1) {
+  array1.push(i);
+  console.log(array1);
+}
+
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+for (i = 0; i < array1.length; i += 1) {
+  let divisao = array1[i] / 2;
+  console.log(divisao);
+}
