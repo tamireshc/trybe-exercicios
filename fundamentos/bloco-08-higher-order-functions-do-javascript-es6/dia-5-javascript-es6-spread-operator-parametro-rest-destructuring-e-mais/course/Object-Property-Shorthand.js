0ca0fcf9fbf14d05a23e1f9680072a45e06affe3
tamireshc <tamireshc@gmail.com>
2022-04-13 10:18:51 -0300

    adiciona exercicios do course dia 5 bloco 8

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/course/Object-Property-Shorthand.js
@@ -0,0 +1,12 @@
+//Agora é hora de praticar: altere a função getPosition utilizando a property shorthand .
+
+// const getPosition = (latitude, longitude) => ({
+//   latitude: latitude,
+//   longitude: longitude});
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
